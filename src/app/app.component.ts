6b1c955d5e66f68047507c9836949ea30168ad03
linroot <linroot@bk.ru>
2024-01-15 21:42:31 +0300

    angular init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,7 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {SportComponent} from "./sport/sport.component";
 import {SeasonComponent} from "./season/season.component";
-import {UiComponent} from "./shared/ui/ui.component";
-import {SharedComponent} from "./shared/shared.component";
 import {SidebarComponent} from "./shared/ui/sidebar/sidebar.component";
-import {TuiSidebarModule} from "@taiga-ui/addon-mobile";
-import {TuiActiveZoneModule} from "@taiga-ui/cdk";
 
 @Component({
   selector: 'app-root',
@@ -17,21 +13,17 @@ import {TuiActiveZoneModule} from "@taiga-ui/cdk";
   imports: [
     CommonModule,
     RouterOutlet,
-    TuiRootModule,
-    TuiDialogModule,
-    TuiAlertModule,
-    TuiSidebarModule,
-    TuiActiveZoneModule,
     HeaderComponent,
     SportComponent,
     SeasonComponent,
-    UiComponent,
-    SharedComponent,
     SidebarComponent,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'statsboards-angular-legacy';
+  title = 'StatsBoards';
 }
