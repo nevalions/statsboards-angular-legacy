10f3f18d0a64fc85bf2376770204ad1bd21218a7
linroot <linroot@bk.ru>
2024-01-15 22:20:05 +0300

    angular init

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,7 @@ import {Component, Input} from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.less'
 })
 
 export class HeaderComponent {
