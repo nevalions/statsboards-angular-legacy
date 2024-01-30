41696a91242b0ac0deae663eb22cdf26807df216
linroot <linroot@bk.ru>
2024-01-30 16:30:41 +0300

    refactoring pagination

--- src/app/components/team/list-of-teams/list-of-teams.component.ts
+++ src/app/components/team/list-of-teams/list-of-teams.component.ts
@@ -1,16 +1,14 @@
-import {ChangeDetectionStrategy, Component, inject, Input, OnInit, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, Input, OnInit} from '@angular/core';
 import {AsyncPipe, SlicePipe} from "@angular/common";
 import {ListOfItemsIslandComponent} from "../../../shared/ui/list-of-items-island/list-of-items-island.component";
 import {TuiButtonModule} from "@taiga-ui/core";
 import {TuiPaginationModule} from "@taiga-ui/kit";
 import {ITeam} from "../../../type/team.type";
-import {BehaviorSubject, combineLatest, map, Observable, of, startWith, switchMap} from "rxjs";
+import {Observable, of} from "rxjs";
 import {
   FormSearchAutoCompleteComponent
 } from "../../../shared/ui/forms/form-search-auto-complete/form-search-auto-complete.component";
 import {FormSearchTextComponent} from "../../../shared/ui/forms/form-search-text/form-search-text.component";
-import {toObservable} from "@angular/core/rxjs-interop";
-import {tap} from "rxjs/operators";
 import {SearchListService} from "../../../services/search-list.service";
 import {PaginationService} from "../../../services/pagination.service";
 
@@ -35,23 +33,11 @@ export class ListOfTeamsComponent implements OnInit{
   searchListService = inject(SearchListService)
   paginationService = inject(PaginationService);
 
-  // @Input() itemsPerPage: number = 4;
-  // @Input() currentPageIndex: BehaviorSubject<number> = new BehaviorSubject(1);
-  // public totalPages: number = 0;
-
   @Input() emptyMessage: string = 'No teams available';
   @Input() teams$: Observable<ITeam[]> = of([]);
-  // paginatedTeams$: Observable<ITeam[]> = of([]);
+
   @Input() formatPath: (item: ITeam) => string = () => '';
   @Input() titleProperty: keyof ITeam  = 'id';
-
-  // protected readonly Math = Math;
-
-  // setPage(pageIndex: number) {
-  //   console.log('Setting page to', pageIndex)
-  //   this.currentPageIndex.next(pageIndex);
-  // }
-
 
   ngOnInit() {
     // update searchListService
@@ -61,42 +47,3 @@ export class ListOfTeamsComponent implements OnInit{
     this.paginationService.initializePagination(this.searchListService.filteredData$);
   }
 }
-
-//   private sliceData(data: Observable<ITeam[]>): Observable<ITeam[]> {
-//     return data.pipe(
-//       tap(teams => console.log('Unpaginated teams:', teams)),
-//       filter(teams => teams.length > 0),
-//       map(teams => {
-//         const start = (this.currentPageIndex - 1) * this.itemsPerPage;
-//         const end = this.currentPageIndex * this.itemsPerPage;
-//         const paginatedTeams = teams.slice(start, end);
-//
-//         console.log('Start:', start);
-//         console.log('End:', end);
-//         console.log('Paginated teams:', paginatedTeams);
-//
-//         return paginatedTeams;
-//       }),
-//     );
-// }
-
-  // stringifyFunc(data: ITeam): string {
-  //   if (data) {
-  //     return data.title.toString();
-  //   }
-  //   else {
-  //     return ''
-  //   }
-  // }
-  //
-  // matcherFunc(data: ITeam, search: string) {
-  //   if(search) {
-  //     return data.title.toLowerCase().startsWith(search)
-  //     }
-  //   return ''
-  // }
-
-
-
-
-
--- src/app/shared/ui/forms/form-search-text/form-search-text.component.ts
+++ src/app/shared/ui/forms/form-search-text/form-search-text.component.ts
@@ -1,11 +1,10 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {TuiInputModule, TuiInputMonthRangeModule} from "@taiga-ui/kit";
 import {UpperCasePipe} from "@angular/common";
-import {debounceTime, distinctUntilChanged, map, Observable, of, startWith, Subscription, switchMap} from "rxjs";
+import {debounceTime, distinctUntilChanged, map, Observable, of, startWith} from "rxjs";
 import {TuiTextfieldControllerModule} from "@taiga-ui/core";
 import {TuiValueChangesModule} from "@taiga-ui/cdk";
-import {tap} from "rxjs/operators";
 import {SearchListService} from "../../../../services/search-list.service";
 
 @Component({
@@ -44,9 +43,6 @@ export class FormSearchTextComponent<T> implements OnInit{
       .subscribe(query => {
         if (query) {
           this.searchListService.updateFilteredData(query, this.parameter);
-        } else {
-          // This will be triggered when the search input is cleared
-          this.searchListService.updateFilteredData('', this.parameter);
         }
       });
   }
@@ -55,35 +51,8 @@ export class FormSearchTextComponent<T> implements OnInit{
     const searchString = this.searchForm.get('searchValue')!.value;
     if (searchString) {
       this.searchListService.updateFilteredData(searchString, this.parameter);
-    }}
+    } else {
+      this.searchListService.updateFilteredData('', this.parameter);
+    }
+  }
 }
-
-//   onSearch(searchString?: string) {
-//   this.subscription?.unsubscribe();
-//
-//   this.subscription = this.data$
-//     .pipe(
-//       map((items) => this.filterItems(items, searchString ?? ''))
-//     )
-//     .subscribe(filteredItems => {
-//       this.searchListService.updateFilteredData(filteredItems);
-//     });
-//   }
-//
-//   private filterItems(items: T[], searchString: string): T[] {
-//     if (searchString === '') {
-//       return items;
-//     }
-//     else {
-//       const lowerCaseSearch = searchString.toLowerCase();
-//       return items.filter((item) => {
-//         const paramValue = String(item[this.parameter as keyof T]).toLowerCase();
-//         return paramValue.startsWith(lowerCaseSearch);
-//       });
-//     }
-//   }
-//
-//   ngOnDestroy() {
-//     this.subscription?.unsubscribe();
-//   }
-// }
