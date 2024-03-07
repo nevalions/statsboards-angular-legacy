e676231db290ad412e563f112db1194595c16338
linroot <linroot@bk.ru>
2024-03-07 22:03:02 +0300

    match scoreboard store

---
+++ src/app/components/scoreboard-data/scoreboard-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scoreboard-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './scoreboard-data.component.html',
+  styleUrl: './scoreboard-data.component.less'
+})
+export class ScoreboardDataComponent {
+
+}
