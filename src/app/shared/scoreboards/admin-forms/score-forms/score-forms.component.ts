b3a628263101803ce7b46019dae4102e030192a0
linroot <linroot@bk.ru>
2024-03-11 11:58:15 +0300

    refactoring admin forms, score

--- src/app/shared/scoreboards/admin-forms/score-forms/score-forms.component.ts
+++ src/app/shared/scoreboards/admin-forms/score-forms/score-forms.component.ts
@@ -1,27 +1,95 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { IMatchData } from '../../../../type/matchdata.type';
 import { MatchData } from '../../../../components/match/matchdata';
 import { AsyncPipe, NgIf } from '@angular/common';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { IMatchFullDataWithScoreboard } from '../../../../type/match.type';
-import { Ui } from '../../../../store/ui/ui';
 import { ToggleVisibleButtonComponent } from '../../../ui/buttons/toggle-visible-button/toggle-visible-button.component';
-import { TuiButtonModule } from '@taiga-ui/core';
+import {
+  TuiButtonModule,
+  TuiErrorModule,
+  TuiTextfieldControllerModule,
+} from '@taiga-ui/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  TuiFieldErrorPipeModule,
+  TuiInputNumberModule,
+  tuiInputNumberOptionsProvider,
+} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-score-forms',
   standalone: true,
-  imports: [AsyncPipe, NgIf, ToggleVisibleButtonComponent, TuiButtonModule],
+  imports: [
+    AsyncPipe,
+    NgIf,
+    ToggleVisibleButtonComponent,
+    TuiButtonModule,
+    ReactiveFormsModule,
+    TuiInputNumberModule,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
+    TuiTextfieldControllerModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    tuiInputNumberOptionsProvider({
+      decimal: 'never',
+      step: 1,
+      min: 0,
+    }),
+  ],
   templateUrl: './score-forms.component.html',
   styleUrl: './score-forms.component.less',
 })
-export class ScoreFormsComponent {
+export class ScoreFormsComponent implements OnChanges {
   @Input() scoreInputsVisible$!: Observable<boolean>;
   @Input() scoreButtonsVisible$!: Observable<boolean>;
   @Input() data!: IMatchFullDataWithScoreboard;
   @Input() disabled: boolean = false;
 
-  constructor(private matchData: MatchData) {}
+  scoreForm: FormGroup;
+
+  constructor(private matchData: MatchData) {
+    this.scoreForm = this.initForm();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data']) {
+      this.scoreForm = this.initForm();
+    }
+    if (changes['disabled']) {
+      if (this.disabled) {
+        this.scoreForm.disable();
+      } else {
+        this.scoreForm.enable();
+      }
+    }
+  }
+
+  private initForm(): FormGroup {
+    return new FormGroup({
+      scoreTeamA: new FormControl<number | null | undefined>(
+        this.data?.match_data?.score_team_a,
+        Validators.min(0),
+      ),
+      scoreTeamB: new FormControl<number | null | undefined>(
+        this.data?.match_data?.score_team_b,
+        Validators.min(0),
+      ),
+    });
+  }
 
   adjustScore(team: 'a' | 'b', amount: number) {
     return (matchData: IMatchData) => {
@@ -35,6 +103,20 @@ export class ScoreFormsComponent {
         this.matchData.updateMatchData(newMatchData);
       }
     };
+  }
+
+  updateScoreTeamA(matchData: IMatchData) {
+    if (!matchData) return;
+
+    if (this.scoreForm.valid) {
+      const formValue = this.scoreForm.getRawValue();
+      const score = Number(formValue.scoreTeamA);
+
+      if (score >= 0) {
+        const updatedMatchData = { ...matchData, score_team_a: score };
+        this.matchData.updateMatchData(updatedMatchData);
+      }
+    }
   }
 
   updateScore(team: 'a' | 'b', inputValue: string) {
