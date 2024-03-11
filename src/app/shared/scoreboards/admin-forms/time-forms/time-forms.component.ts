5dc9d33613e7aa36b4c94cff870470d40a0de1cb
linroot <linroot@bk.ru>
2024-03-11 22:01:14 +0300

    refactoring admin forms time

--- src/app/shared/scoreboards/admin-forms/time-forms/time-forms.component.ts
+++ src/app/shared/scoreboards/admin-forms/time-forms/time-forms.component.ts
@@ -1,24 +1,125 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Observable } from 'rxjs';
 import { IMatchFullDataWithScoreboard } from '../../../../type/match.type';
 import { MatchData } from '../../../../components/match/matchdata';
 import { ToggleVisibleButtonComponent } from '../../../ui/buttons/toggle-visible-button/toggle-visible-button.component';
 import { IMatchData } from '../../../../type/matchdata.type';
+import {
+  TuiFieldErrorPipeModule,
+  TuiInputModule,
+  TuiInputNumberModule,
+  tuiInputNumberOptionsProvider,
+} from '@taiga-ui/kit';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TuiErrorModule } from '@taiga-ui/core';
+import { AdminSubmitButtonComponent } from '../../../ui/buttons/admin-submit-button/admin-submit-button.component';
 
 @Component({
   selector: 'app-time-forms',
   standalone: true,
-  imports: [NgIf, AsyncPipe, ToggleVisibleButtonComponent],
+  imports: [
+    NgIf,
+    AsyncPipe,
+    ToggleVisibleButtonComponent,
+    ReactiveFormsModule,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
+    TuiInputNumberModule,
+    TuiInputModule,
+    AdminSubmitButtonComponent,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    tuiInputNumberOptionsProvider({
+      decimal: 'never',
+      step: 1,
+      min: 0,
+    }),
+  ],
   templateUrl: './time-forms.component.html',
   styleUrl: './time-forms.component.less',
 })
-export class TimeFormsComponent {
+export class TimeFormsComponent implements OnChanges {
   @Input() timeFormsVisible$!: Observable<boolean>;
   @Input() data!: IMatchFullDataWithScoreboard;
-  @Input() isMatchDataSubmitting$?: Observable<boolean>;
+  @Input() disabled: boolean = false;
 
-  constructor(private matchData: MatchData) {}
+  timeForm: FormGroup;
+
+  constructor(private matchData: MatchData) {
+    this.timeForm = this.initForm();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data']) {
+      this.timeForm = this.initForm();
+      if (this.data.match_data?.gameclock_status === 'running') {
+        this.timeForm.get('gameTimeMinutes')?.disable();
+        this.timeForm.get('gameTimeSeconds')?.disable();
+      }
+    }
+
+    if (changes['disabled']) {
+      if (this.disabled) {
+        this.timeForm.disable();
+      } else {
+        this.timeForm.get('gameTimeMinutes')?.enable();
+        this.timeForm.get('gameTimeSeconds')?.enable();
+      }
+    }
+
+    this.timeForm.get('playTimeSeconds')!.disable();
+  }
+
+  private initForm(): FormGroup {
+    const time = this.data?.match_data?.gameclock;
+
+    if (time) {
+      let gameMinutes = Math.floor(time / 60);
+      let gameSeconds = time % 60;
+      return new FormGroup({
+        gameTimeMinutes: new FormControl<number | null | undefined>(
+          gameMinutes,
+          [Validators.min(0), Validators.max(59)],
+        ),
+        gameTimeSeconds: new FormControl<number | null | undefined>(
+          gameSeconds,
+          [Validators.min(0), Validators.max(59)],
+        ),
+        playTimeSeconds: new FormControl<number | null | undefined>(
+          this.data?.match_data?.playclock,
+          Validators.min(0),
+        ),
+      });
+    } else {
+      return new FormGroup({
+        gameTimeMinutes: new FormControl<number | null | undefined>(null, [
+          Validators.min(0),
+          Validators.max(59),
+        ]),
+        gameTimeSeconds: new FormControl<number | null | undefined>(null, [
+          Validators.min(0),
+          Validators.max(59),
+        ]),
+        playTimeSeconds: new FormControl<number | null | undefined>(
+          null,
+          Validators.min(0),
+        ),
+      });
+    }
+  }
 
   startGameClock() {
     this.matchData.startGameClock();
@@ -30,17 +131,21 @@ export class TimeFormsComponent {
 
   saveNewGameClock(
     matchData: IMatchData,
-    min: string | number,
-    sec: string | number,
+    // min: string | number,
+    // sec: string | number,
   ) {
     if (!matchData) return;
 
-    const minutes = Number(min);
-    const seconds = Number(sec);
-    if (minutes && seconds) {
-      const time: number = minutes * 60 + seconds;
-      const updatedMatchData = { ...matchData, gameclock: time };
-      this.matchData.updateMatchData(updatedMatchData);
+    if (this.timeForm.valid) {
+      const formValue = this.timeForm.getRawValue();
+
+      const minutes = Number(formValue.gameTimeMinutes);
+      const seconds = Number(formValue.gameTimeSeconds);
+      if (minutes && seconds) {
+        const time: number = minutes * 60 + seconds;
+        const updatedMatchData = { ...matchData, gameclock: time };
+        this.matchData.updateMatchData(updatedMatchData);
+      }
     }
   }
 
@@ -56,29 +161,29 @@ export class TimeFormsComponent {
     this.matchData.resetPlayClock();
   }
 
-  getMinutes(seconds: number): string {
-    if (seconds === undefined) {
-      return '--';
-    } else {
-      return Math.floor(seconds / 60)
-        .toString()
-        .padStart(2, '0');
-    }
-  }
-
-  getSeconds(seconds: number): string {
-    if (seconds === undefined) {
-      return '--';
-    } else {
-      return (seconds % 60).toString().padStart(2, '0');
-    }
-  }
-
-  getPlayClockSeconds(seconds: number): string {
-    if (seconds === undefined) {
-      return '';
-    } else {
-      return (seconds % 60).toString().padStart(1, '0');
-    }
-  }
+  // getMinutes(seconds: number): string {
+  //   if (seconds === undefined) {
+  //     return '--';
+  //   } else {
+  //     return Math.floor(seconds / 60)
+  //       .toString()
+  //       .padStart(2, '0');
+  //   }
+  // }
+  //
+  // getSeconds(seconds: number): string {
+  //   if (seconds === undefined) {
+  //     return '--';
+  //   } else {
+  //     return (seconds % 60).toString().padStart(2, '0');
+  //   }
+  // }
+  //
+  // getPlayClockSeconds(seconds: number): string {
+  //   if (seconds === undefined) {
+  //     return '';
+  //   } else {
+  //     return (seconds % 60).toString().padStart(1, '0');
+  //   }
+  // }
 }
