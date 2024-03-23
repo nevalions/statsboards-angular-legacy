cf35beaa49fc3fe9c4035ddc3b4b162c69eabe0e
linroot <linroot@bk.ru>
2024-03-23 19:09:30 +0300

    run prod server

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -12,7 +12,7 @@ export const AuthInterceptor: HttpInterceptorFn = (
   const token: string | null = '12345';
   let serverIP = environment.url;
   let serverPort = environment.port;
-  let serverProtocol = environment.backendProtocol;
+  let serverProtocol = environment.protocol;
   // check for token in storage
   // console.log(serverIP, serverIP);
   if (token) {
