df0c4039add739f0d16d71e7477bdce6b59cad22
linroot <linroot@bk.ru>
2024-03-28 12:53:58 +0300

    breads refactored layout

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Breadcrumb } from '../../type/base.type';
 import { Store } from '@ngrx/store';
@@ -6,8 +6,19 @@ import { AppState } from '../../store/appstate';
 import { AsyncPipe } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { TuiBreadcrumbsModule } from '@taiga-ui/kit';
-import { TuiLinkModule, TuiLoaderModule, TuiRootModule } from '@taiga-ui/core';
+import {
+  TuiBreadcrumbsModule,
+  tuiBreadcrumbsOptionsProvider,
+} from '@taiga-ui/kit';
+import {
+  TuiButtonModule,
+  TuiDataListModule,
+  TuiHostedDropdownModule,
+  TuiLinkModule,
+  TuiLoaderModule,
+  TuiRootModule,
+} from '@taiga-ui/core';
+import { TuiRepeatTimesModule } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-layout',
@@ -21,13 +32,26 @@ import { TuiLinkModule, TuiLoaderModule, TuiRootModule } from '@taiga-ui/core';
     TuiLoaderModule,
     TuiRootModule,
     RouterLink,
+    TuiRepeatTimesModule,
+    TuiHostedDropdownModule,
+    TuiDataListModule,
+    TuiButtonModule,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    tuiBreadcrumbsOptionsProvider({
+      icon: 'tuiIconArrowRight',
+      mode: null,
+      size: 'l',
+    }),
+  ],
 })
 export class LayoutComponent {
   title = 'StatsBoards';
   breadcrumbs$: Observable<Breadcrumb[]>;
+  maxBreads: number = 4;
 
   constructor(private store: Store<AppState>) {
     this.breadcrumbs$ = this.store.select(
