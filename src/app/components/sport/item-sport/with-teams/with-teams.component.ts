7630468c0b713e256bcb0ef69e0d2f639ab49d44
linroot <linroot@bk.ru>
2024-05-05 12:48:24 +0300

    refactoring

--- src/app/components/sport/item-sport/with-teams/with-teams.component.ts
+++ src/app/components/sport/item-sport/with-teams/with-teams.component.ts
@@ -59,21 +59,10 @@ export class WithTeamsComponent {
   sport$ = this.sport.currentSport$;
   allSponsors$ = this.sponsor.allSponsors$;
   allSponsorLines$ = this.sponsorLine.allSponsorLines$;
-  teamInSportSearchResults$ = this.search.teamInSportSearchResults$;
   paginatedTeamInSportSearchResults$ =
     this.pagination.paginatedTeamInSportSearchResults$;
   currentPage$ = this.pagination.currentPage$;
-  itemsPerPage$ = this.pagination.itemsPerPage$;
   totalTeamInSportSearchPages$ = this.pagination.totalTeamInSportSearchPages$;
-
-  // itemPerPageForm: FormGroup = new FormGroup({
-  //   itemPerPageValue: new FormControl(4, [
-  //     Validators.min(2),
-  //     // Validators.max(20),
-  //   ]),
-  // });
-  //
-  // itemsPerPage = ['All', 2, 4, 6, 8, 10, 20];
 
   constructor(
     private sport: Sport,
@@ -88,13 +77,6 @@ export class WithTeamsComponent {
     team.loadAllTeamsInSport();
 
     this.search.searchTeamInSport(null);
-
-    // this.itemPerPageForm
-    //   .get('itemPerPageValue')!
-    //   .valueChanges.pipe(debounceTime(300), distinctUntilChanged())
-    //   .subscribe((itemsPerPage) => {
-    //     this.pagination.changeItemsPerPage(itemsPerPage);
-    //   });
   }
 
   islandTeamTitleProperty: keyof ITeam = 'title';
