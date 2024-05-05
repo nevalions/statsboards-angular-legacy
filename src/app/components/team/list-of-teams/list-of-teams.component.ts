a0694d9219e31489dd439df0bd6409799102fa56
linroot <linroot@bk.ru>
2024-05-05 13:03:38 +0300

    list of teams refactor

--- src/app/components/team/list-of-teams/list-of-teams.component.ts
+++ src/app/components/team/list-of-teams/list-of-teams.component.ts
@@ -1,17 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TuiLoaderModule } from '@taiga-ui/core';
 import { ITeam } from '../../../type/team.type';
-import { AddEditTeamComponent } from '../add-edit-team/add-edit-team.component';
 import { ListOfTeamsWithCityComponent } from '../list-of-teams-with-city/list-of-teams-with-city.component';
 
 @Component({
   selector: 'app-list-of-teams',
   standalone: true,
-  imports: [
-    AddEditTeamComponent,
-    TuiLoaderModule,
-    ListOfTeamsWithCityComponent,
-  ],
+  imports: [TuiLoaderModule, ListOfTeamsWithCityComponent],
 
   templateUrl: './list-of-teams.component.html',
   styleUrl: './list-of-teams.component.less',
@@ -25,22 +20,4 @@ export class ListOfTeamsComponent {
 
   @Input() formatPath: (item: ITeam) => string = () => '';
   @Input() titleProperty: keyof ITeam = 'id';
-
-  // searchForm = new FormGroup({
-  //   searchValue: new FormControl(''),
-  // });
-  //
-  // onSearch() {}
-
-  // searchListService = inject(SearchListService);
-  // paginationService = inject(PaginationService);
-  // teamService = inject(TeamService);
-  // teams$ = this.team.teamsInSport$;
-
-  // ngOnInit() {
-  //   this.searchListService.updateData(this.teams$);
-  //   this.paginationService.initializePagination(
-  //     this.searchListService.filteredData$,
-  //   );
-  // }
 }
