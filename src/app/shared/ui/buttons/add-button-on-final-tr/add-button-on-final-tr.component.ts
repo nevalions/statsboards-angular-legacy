3254b234b531a7f30e23c6ee4d71cbd35f8ac023
linroot <linroot@bk.ru>
2024-05-15 15:06:16 +0300

    table with players refactoring

--- src/app/shared/ui/buttons/add-button-on-final-tr/add-button-on-final-tr.component.ts
+++ src/app/shared/ui/buttons/add-button-on-final-tr/add-button-on-final-tr.component.ts
@@ -1,15 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ButtonIconComponent } from '../button-icon/button-icon.component';
 import { TuiTableModule } from '@taiga-ui/addon-table';
+import { CreateButtonInFormComponent } from '../create-button-in-form/create-button-in-form.component';
 
 @Component({
   selector: 'app-add-button-on-final-tr',
   standalone: true,
-  imports: [ButtonIconComponent, TuiTableModule],
+  imports: [ButtonIconComponent, TuiTableModule, CreateButtonInFormComponent],
   templateUrl: './add-button-on-final-tr.component.html',
   styleUrl: './add-button-on-final-tr.component.less',
 })
 export class AddButtonOnFinalTrComponent {
+  @Input() action: string = 'add';
   @Output() onAdd = new EventEmitter();
 
   add() {
