b9ea98df6581b2bb02a1cb32e204cd0d7147f9de
nevalions <nevalions@gmail.com>
2024-08-05 19:10:09 +0300

    football events calculate on td

--- src/app/components/match-event/football-event/store/selectors.ts
+++ src/app/components/match-event/football-event/store/selectors.ts
@@ -39,6 +39,7 @@ export function calculateDistanceMoved(
     event.play_result &&
     event.score_result !== IFootballScoreResult.Td &&
     event.score_result !== IFootballScoreResult.TdDefence &&
+    event.score_result !== IFootballScoreResult.Safety &&
     (event.play_type === IFootballPlayType.Pass ||
       event.play_type === IFootballPlayType.Run) &&
     event.play_result !== IFootballPlayResult.Flag &&
