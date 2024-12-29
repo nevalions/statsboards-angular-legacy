9bface6edff71f0a3d4f6e23b51dad11b769edb8
linroot <nevalions@live.com>
2024-12-29 20:44:08 +0300

    test env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,30 +1,15 @@
-// let protocol = 'https';
-// let apiUrl = 'statsboard.ru';
-
-// export const environment = {
-//   production: true,
-//   currentYear: new Date().getFullYear(),
-//   currentSeasonId: 2,
-//   url: 'statsboard.ru',
-//   port: ':9000',
-//   angular_port: '',
-//   protocol: protocol,
-//   wsProtocol: 'wss',
-//   apiUrl: apiUrl,
-//   backendUrl: protocol + '://' + apiUrl,
-// };
 let protocol = 'https';
-let apiUrl = '0.0.0.0:9000';
+let apiUrl = 'statsboard.ru';
 
 export const environment = {
   production: true,
   currentYear: new Date().getFullYear(),
   currentSeasonId: 2,
-  url: 'localhost',
+  url: 'statsboard.ru',
   port: ':9000',
   angular_port: '',
   protocol: protocol,
-  wsProtocol: 'ws',
+  wsProtocol: 'wss',
   apiUrl: apiUrl,
   backendUrl: protocol + '://' + apiUrl,
 };
