b9859f3ace30072ae2c61b76977ed6deaf5425b5
linroot <nevalions@live.com>
2025-02-06 17:18:06 +0300

    production dockerfile build for kuber

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,17 @@
-let protocol = 'https';
-let apiUrl = 'statsboard.ru';
+// let protocol = 'https';
+// let apiUrl = 'statsboard.ru';
+
+let protocol = 'http';
+let apiUrl = '213.171.10.112';
 
 export const environment = {
   production: true,
   currentYear: new Date().getFullYear(),
   currentSeasonId: 2,
-  url: 'statsboard.ru',
+  url: apiUrl,
   port: ':9000',
+  // url: 'statsboard.ru',
+  // port: ':9000',
   angular_port: '',
   protocol: protocol,
   wsProtocol: 'wss',
