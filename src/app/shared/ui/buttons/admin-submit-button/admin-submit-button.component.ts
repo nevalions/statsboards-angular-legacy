77046f0f2b4ff0381dd1c57a5658d7830a8bd7ed
linroot <nevalions@gmail.com>
2025-03-27 15:01:52 +0300

    import refactored

--- src/app/shared/ui/buttons/admin-submit-button/admin-submit-button.component.ts
+++ src/app/shared/ui/buttons/admin-submit-button/admin-submit-button.component.ts
@@ -1,13 +1,11 @@
 import { TuiButton } from "@taiga-ui/core";
 import { Component, Input } from '@angular/core';
-import { AddSignPipe } from '../../../../pipes/add-sign.pipe';
-import { TeamNamePipe } from '../../../../pipes/team-name.pipe';
 import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-admin-submit-button',
   standalone: true,
-  imports: [AddSignPipe, TeamNamePipe, TuiButton, TitleCasePipe],
+  imports: [TuiButton, TitleCasePipe],
   templateUrl: './admin-submit-button.component.html',
   styleUrl: './admin-submit-button.component.less',
 })
