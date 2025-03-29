174a684de4058b83ec7d2ad53df4b84512d0dddd
linroot <nevalions@gmail.com>
2025-03-29 19:17:00 +0300

    changed from island to card

--- src/app/components/player/list-of-players/list-of-players.component.ts
+++ src/app/components/player/list-of-players/list-of-players.component.ts
@@ -1,21 +1,47 @@
-import { TuiFallbackSrcPipe, TuiInitialsPipe } from "@taiga-ui/core";
+import { TuiAppearance, TuiFallbackSrcPipe, TuiInitialsPipe, TuiLoader, TuiSurface, TuiTitle } from "@taiga-ui/core";
 import { TuiAvatar } from "@taiga-ui/kit";
 import { TuiIslandDirective } from "@taiga-ui/legacy";
 import { Component, Input } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { IPlayer, IPlayerInSport } from '../../../type/player.type';
 import { environment } from '../../../../environments/environment';
+import { TuiCardLarge, TuiCell } from "@taiga-ui/layout";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-list-of-players',
   standalone: true,
-  imports: [TitleCasePipe, TuiAvatar, TuiIslandDirective, TuiFallbackSrcPipe, TuiInitialsPipe],
+  imports: [
+    TuiCardLarge,
+    TuiCell,
+    TuiTitle,
+    TuiSurface,
+    TuiAppearance,
+    TitleCasePipe,
+    TuiAvatar,
+    TuiLoader,
+    TitleCasePipe,
+    TuiAvatar,
+  ],
   templateUrl: './list-of-players.component.html',
   styleUrl: './list-of-players.component.less',
 })
 export class ListOfPlayersComponent {
   @Input() players: IPlayerInSport[] = [];
   @Input() playerItemHref: (item: IPlayer) => string = () => '';
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  // TODO: move to helper function
+  navigateToItem(urlItem: string): void {
+    if (urlItem) {
+      const segments = urlItem.split('/');
+      this.router.navigate(segments);
+    }
+  }
+
 
   backendUrl = environment.backendUrl;
 }
