504acf3524f281e36063a53391578afa7361d84d
linroot <nevalions@gmail.com>
2025-03-29 18:57:32 +0300

    refactor

--- src/app/shared/scoreboards/team-match-lower-football-stats-display-flat/team-match-lower-football-stats-display-flat.component.ts
+++ src/app/shared/scoreboards/team-match-lower-football-stats-display-flat/team-match-lower-football-stats-display-flat.component.ts
@@ -1,15 +1,12 @@
-import { TuiAvatar } from "@taiga-ui/kit";
 import { Component, Input } from '@angular/core';
 import { ImageService } from '../../../services/image.service';
 import { environment } from '../../../../environments/environment';
 import { hexToRgba } from '../../../base/helpers';
 import { IFootballTeamWithStats } from '../../../type/team.type';
-import { AsyncPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-team-match-lower-football-stats-display-flat',
   standalone: true,
-  imports: [AsyncPipe, TuiAvatar, UpperCasePipe],
   templateUrl: './team-match-lower-football-stats-display-flat.component.html',
   styleUrl: './team-match-lower-football-stats-display-flat.component.less',
 })
@@ -17,7 +14,7 @@ export class TeamMatchLowerFootballStatsDisplayFlatComponent {
   @Input() footballTeamWithStats: IFootballTeamWithStats | null = null;
   @Input() teamColor: string = '#3b3b3b';
 
-  constructor(private imageService: ImageService) {}
+  constructor(private imageService: ImageService) { }
 
   onImgError(event: Event) {
     this.imageService.handleError(event);
