9edd477c3300ff1aebae373e69c989e28eb5dd76
linroot <nevalions@gmail.com>
2025-03-29 12:57:51 +0300

    change from island to card

--- src/app/components/tournament/island-list-of-tournaments/island-list-of-tournaments.component.ts
+++ src/app/components/tournament/island-list-of-tournaments/island-list-of-tournaments.component.ts
@@ -1,45 +1,39 @@
 import { TuiAvatar } from "@taiga-ui/kit";
-import { TuiIslandDirective } from "@taiga-ui/legacy";
 import { Component, Input } from '@angular/core';
-import { AsyncPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { TuiSizeL, TuiSizeS, TuiLoader, TuiFallbackSrcPipe, TuiInitialsPipe } from '@taiga-ui/core';
+import { TuiTitle, TuiSurface, TuiAppearance, TuiIcon } from '@taiga-ui/core';
+import { TuiCardLarge, TuiCell, TuiHeader } from '@taiga-ui/layout';
+
 import { ITournament } from '../../../type/tournament.type';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { getTitleCase } from '../../../base/helpers';
 import { environment } from '../../../../environments/environment';
 import { urlWithProtocol } from "../../../base/constants";
+import { UpperCasePipe } from "@angular/common";
 
 @Component({
   selector: 'app-island-list-of-tournaments',
   standalone: true,
   imports: [
-    AsyncPipe,
-    TuiIslandDirective,
-    TuiLoader,
-    TitleCasePipe,
-    UpperCasePipe,
+    TuiCardLarge,
+    TuiHeader,
+    TuiCell,
+    TuiTitle,
+    TuiIcon,
+    TuiSurface,
+    TuiAppearance,
     TuiAvatar,
-    TuiFallbackSrcPipe,
-    TuiInitialsPipe
+    UpperCasePipe,
   ],
   templateUrl: './island-list-of-tournaments.component.html',
   styleUrl: './island-list-of-tournaments.component.less',
 })
 export class IslandListOfTournamentsComponent {
-  @Input() emptyMessage: string = 'No data available';
-  @Input() titleProperty: keyof ITournament = 'title';
-  @Input() paragraphProperty: keyof ITournament = 'description';
-  @Input() avatarUrl: keyof ITournament = 'title';
-
   @Input() data: ITournament[] = [];
-  @Input() _size: TuiSizeL | TuiSizeS = 'l';
-  @Input() hoverable: boolean = true;
 
   backendUrl = environment.backendUrl;
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute,
   ) { }
 
   navigateToTournamentItem(item: ITournament): void {
@@ -49,11 +43,6 @@ export class IslandListOfTournamentsComponent {
     }
     currentUrl.push('tournament', item.id!.toString());
     this.router.navigate(currentUrl);
-  }
-
-  getInitialsText(item: any): string {
-    const text = item[this.titleProperty] || 'image';
-    return text?.toString() || '';
   }
 
   protected readonly url = urlWithProtocol;
