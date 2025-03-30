5f5bb47af97ccfab5ec24845d87899eabb499796
linroot <nevalions@gmail.com>
2025-03-30 12:31:01 +0300

    logo repaired

--- src/app/components/adv/sponsor/sponsor.component.ts
+++ src/app/components/adv/sponsor/sponsor.component.ts
@@ -5,13 +5,13 @@ import {
 } from '@angular/core';
 import { Sponsor } from './sponsor';
 import { ListOfItemsIslandComponent } from '../../../shared/ui/list/list-of-items-island/list-of-items-island.component';
-import { AsyncPipe, UpperCasePipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { ISponsor } from '../../../type/sponsor.type';
 
 @Component({
   selector: 'app-store',
   standalone: true,
-  imports: [ListOfItemsIslandComponent, AsyncPipe, UpperCasePipe],
+  imports: [ListOfItemsIslandComponent, AsyncPipe,],
   templateUrl: './sponsor.component.html',
   styleUrl: './sponsor.component.less',
   encapsulation: ViewEncapsulation.None,
