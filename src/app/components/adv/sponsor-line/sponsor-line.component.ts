279477aa342287718e07a938c31d03ce8f318927
linroot <nevalions@gmail.com>
2025-03-30 12:31:23 +0300

    todo logo show for sponsorline

--- src/app/components/adv/sponsor-line/sponsor-line.component.ts
+++ src/app/components/adv/sponsor-line/sponsor-line.component.ts
@@ -4,14 +4,14 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { ListOfItemsIslandComponent } from '../../../shared/ui/list/list-of-items-island/list-of-items-island.component';
-import { AsyncPipe, UpperCasePipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { SponsorLine } from './sponsorLine';
 import { ISponsor } from '../../../type/sponsor.type';
 
 @Component({
   selector: 'app-store-line',
   standalone: true,
-  imports: [ListOfItemsIslandComponent, AsyncPipe, UpperCasePipe],
+  imports: [ListOfItemsIslandComponent, AsyncPipe],
   templateUrl: './sponsor-line.component.html',
   styleUrl: './sponsor-line.component.less',
   encapsulation: ViewEncapsulation.None,
@@ -23,7 +23,7 @@ export class SponsorLineComponent {
   constructor(private sponsorLine: SponsorLine) {
     sponsorLine.loadAllSponsorLines();
   }
-
+  // TODO: Change from ISponsor to ISponsorLineWithFullData
   islandTitleProperty: keyof ISponsor = 'title';
 
   itemHref(item: ISponsor): string {
