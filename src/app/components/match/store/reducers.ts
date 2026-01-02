a03ed6967899d99f6d5afeac6a4cb6fe3dfd4078
linroot <nevalions@gmail.com>
2026-01-02 17:40:13 +0300

    STAF-21: Migrate remaining complex entity stores to NgRx Entity adapters

--- src/app/components/match/store/reducers.ts
+++ src/app/components/match/store/reducers.ts
@@ -1,15 +1,14 @@
-import { createFeature, createReducer, on } from '@ngrx/store';
-import { SortService } from '../../../services/sort.service';
+import { createEntityAdapter, EntityState } from '@ngrx/entity';
+import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
 import { matchActions } from './actions';
 import { IMatch, IMatchWithFullData } from '../../../type/match.type';
 import { IPlayerInTeamTournament } from '../../../type/player.type';
 
-export interface MatchState {
+export interface MatchState extends EntityState<IMatch> {
   matchIsLoading: boolean;
   matchIsSubmitting: boolean;
   currentMatchId: number | undefined | null;
   currentMatch: IMatch | undefined | null;
-  allMatches: IMatch[];
   allMatchesInSport: IMatch[];
   allMatchesInTournament: IMatch[];
   allMatchesWithFullDataInTournament: IMatchWithFullData[];
@@ -18,11 +17,21 @@ export interface MatchState {
   errors: any | null;
 }
 
-const initialState: MatchState = {
+const adapter = createEntityAdapter<IMatch>({
+  sortComparer: (a, b) => {
+    if (a.week !== b.week) {
+      return a.week - b.week;
+    }
+    const dateA = a.match_date ? new Date(a.match_date).getTime() : 0;
+    const dateB = b.match_date ? new Date(b.match_date).getTime() : 0;
+    return dateB - dateA;
+  },
+});
+
+const initialState: MatchState = adapter.getInitialState({
   matchIsLoading: false,
   matchIsSubmitting: false,
   currentMatchId: null,
-  allMatches: [],
   allMatchesInSport: [],
   allMatchesInTournament: [],
   allMatchesInTournamentPaginated: [],
@@ -30,201 +39,277 @@ const initialState: MatchState = {
   currentMatch: null,
   parsedMatchesFromTournamentEESL: [],
   errors: null,
-};
+});
 
 const matchFeature = createFeature({
   name: 'match',
   reducer: createReducer(
     initialState,
-    on(matchActions.getId, (state): MatchState => ({
-      ...state,
-      matchIsLoading: true,
-    })),
-    on(matchActions.getMatchIdSuccessfully, (state, action): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-      currentMatchId: action.matchId,
-    })),
-    on(matchActions.getMatchIdFailure, (state): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-    })),
+    on(
+      matchActions.getId,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: true,
+      }),
+    ),
+    on(
+      matchActions.getMatchIdSuccessfully,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsLoading: false,
+        currentMatchId: action.matchId,
+      }),
+    ),
+    on(
+      matchActions.getMatchIdFailure,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: false,
+      }),
+    ),
 
-    // create actions
-    on(matchActions.create, (state): MatchState => ({
-      ...state,
-      matchIsSubmitting: true,
-    })),
+    on(
+      matchActions.create,
+      (state): MatchState => ({
+        ...state,
+        matchIsSubmitting: true,
+      }),
+    ),
     on(matchActions.createdSuccessfully, (state, action) => {
-      const newList = [...state.allMatches, action.currentMatch];
-      const sortedTournaments = SortService.sort(newList, 'week', '-date');
-      return {
+      return adapter.addOne(action.currentMatch, {
         ...state,
         matchIsSubmitting: false,
         currentMatch: action.currentMatch,
-        allMatches: sortedTournaments,
-        allMatchesInTournament: sortedTournaments,
-      };
+        allMatchesInTournament: [
+          ...state.allMatchesInTournament,
+          action.currentMatch,
+        ],
+      });
     }),
-    on(matchActions.createFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsSubmitting: false,
-      errors: action,
-    })),
+    on(
+      matchActions.createFailure,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsSubmitting: false,
+        errors: action,
+      }),
+    ),
 
-    // delete actions
-    on(matchActions.delete, (state): MatchState => ({
-      ...state,
-      matchIsSubmitting: true,
-    })),
-    on(matchActions.deletedSuccessfully, (state, action): MatchState => ({
-      ...state,
-      matchIsSubmitting: false,
-      allMatches: (state.allMatches || []).filter(
-        (item) => item.id !== action.matchId,
-      ),
-      allMatchesInTournament: (state.allMatchesInTournament || []).filter(
-        (item) => item.id !== action.matchId,
-      ),
-      errors: null,
-    })),
-    on(matchActions.deleteFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsSubmitting: false,
-      errors: action,
-    })),
+    on(
+      matchActions.delete,
+      (state): MatchState => ({
+        ...state,
+        matchIsSubmitting: true,
+      }),
+    ),
+    on(matchActions.deletedSuccessfully, (state, action): MatchState => {
+      return adapter.removeOne(action.matchId, {
+        ...state,
+        matchIsSubmitting: false,
+        allMatchesInTournament: state.allMatchesInTournament.filter(
+          (item) => item.id !== action.matchId,
+        ),
+        errors: null,
+      });
+    }),
+    on(
+      matchActions.deleteFailure,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsSubmitting: false,
+        errors: action,
+      }),
+    ),
 
-    // update actions
-    on(matchActions.update, (state): MatchState => ({
-      ...state,
-      matchIsSubmitting: true,
-    })),
-    on(matchActions.updatedSuccessfully, (state, action): MatchState => ({
-      ...state,
-      matchIsSubmitting: false,
-      currentMatch: action.updatedMatch,
-      allMatches: state.allMatches.map((item) =>
-        item.id === action.updatedMatch.id ? action.updatedMatch : item,
-      ),
-      allMatchesInTournament: state.allMatchesInTournament.map((item) =>
-        item.id === action.updatedMatch.id ? action.updatedMatch : item,
-      ),
-      errors: null,
-    })),
-    on(matchActions.updateFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsSubmitting: false,
-      errors: action,
-    })),
+    on(
+      matchActions.update,
+      (state): MatchState => ({
+        ...state,
+        matchIsSubmitting: true,
+      }),
+    ),
+    on(matchActions.updatedSuccessfully, (state, action): MatchState => {
+      return adapter.updateOne(
+        { id: action.updatedMatch.id!, changes: action.updatedMatch },
+        {
+          ...state,
+          matchIsSubmitting: false,
+          currentMatch: action.updatedMatch,
+          allMatchesInTournament: state.allMatchesInTournament.map((item) =>
+            item.id === action.updatedMatch.id ? action.updatedMatch : item,
+          ),
+          errors: null,
+        },
+      );
+    }),
+    on(
+      matchActions.updateFailure,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsSubmitting: false,
+        errors: action,
+      }),
+    ),
     on(matchActions.updateAllMatchesInTournament, (state, { newMatch }) => {
-      const newList = [...state.allMatchesInTournament, newMatch];
-      const sortedMatches = SortService.sort(newList, 'week', '-date');
-      return {
+      return adapter.addOne(newMatch, {
         ...state,
-        allMatchesInSport: sortedMatches,
-      };
+        allMatchesInSport: [...state.allMatchesInSport, newMatch],
+      });
     }),
 
-    // get actions
-    on(matchActions.get, (state): MatchState => ({
-      ...state,
-      matchIsLoading: true,
-    })),
-    on(matchActions.getItemSuccess, (state, action): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-      currentMatch: action.match,
-    })),
-    on(matchActions.getItemFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-      errors: action,
-    })),
+    on(
+      matchActions.get,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: true,
+      }),
+    ),
+    on(
+      matchActions.getItemSuccess,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsLoading: false,
+        currentMatch: action.match,
+      }),
+    ),
+    on(
+      matchActions.getItemFailure,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsLoading: false,
+        errors: action,
+      }),
+    ),
 
-    on(matchActions.getAll, (state): MatchState => ({
-      ...state,
-      matchIsLoading: true,
-    })),
+    on(
+      matchActions.getAll,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: true,
+      }),
+    ),
     on(matchActions.getAllItemsSuccess, (state, action) => {
-      const sortedMatches = SortService.sort(action.matches, '-date');
-      return {
+      return adapter.setAll(action.matches, {
         ...state,
         matchIsLoading: false,
-        allMatches: sortedMatches,
-      };
+      });
     }),
-    on(matchActions.getAllItemsFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-      errors: action,
-    })),
+    on(
+      matchActions.getAllItemsFailure,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsLoading: false,
+        errors: action,
+      }),
+    ),
 
-    on(matchActions.getMatchesBySportId, (state): MatchState => ({
-      ...state,
-      matchIsLoading: true,
-    })),
+    on(
+      matchActions.getMatchesBySportId,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: true,
+      }),
+    ),
     on(matchActions.getMatchesBySportIDSuccess, (state, action) => {
-      const sortedMatches = SortService.sort(action.matches, '-date');
+      const sortedMatches = [...action.matches].sort((a, b) => {
+        const dateA = a.match_date ? new Date(a.match_date).getTime() : 0;
+        const dateB = b.match_date ? new Date(b.match_date).getTime() : 0;
+        return dateB - dateA;
+      });
       return {
         ...state,
         matchIsLoading: false,
         allMatchesInSport: sortedMatches,
       };
     }),
-    on(matchActions.getMatchesBySportIDFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-      errors: action,
-    })),
+    on(
+      matchActions.getMatchesBySportIDFailure,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsLoading: false,
+        errors: action,
+      }),
+    ),
 
-    on(matchActions.getMatchesByTournamentId, (state): MatchState => ({
-      ...state,
-      matchIsLoading: true,
-    })),
+    on(
+      matchActions.getMatchesByTournamentId,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: true,
+      }),
+    ),
     on(matchActions.getMatchesByTournamentIDSuccess, (state, action) => {
-      const sortedMatches = SortService.sort(action.matches, 'week', '-date');
+      const sortedMatches = [...action.matches].sort((a, b) => {
+        if (a.week !== b.week) {
+          return a.week - b.week;
+        }
+        const dateA = a.match_date ? new Date(a.match_date).getTime() : 0;
+        const dateB = b.match_date ? new Date(b.match_date).getTime() : 0;
+        return dateB - dateA;
+      });
       return {
         ...state,
         matchIsLoading: false,
         allMatchesInTournament: sortedMatches,
       };
     }),
-    on(matchActions.getMatchesByTournamentIDFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-      errors: action,
-    })),
-
-    on(matchActions.getMatchesByTournamentIdWithPagination, (state): MatchState => ({
-      ...state,
-      matchIsLoading: true,
-    })),
-    on(matchActions.getMatchesByTournamentIDWithPaginationSuccess, (state, action) => {
-      const sortedMatches = SortService.sort(action.matches, 'week', '-date');
-      return {
+    on(
+      matchActions.getMatchesByTournamentIDFailure,
+      (state, action): MatchState => ({
         ...state,
         matchIsLoading: false,
-        allMatchesInTournamentPaginated: sortedMatches,
-      };
-    }),
-    on(matchActions.getMatchesByTournamentIDWithPaginationFailure, (state, action): MatchState => ({
-      ...state,
-      matchIsLoading: false,
-      errors: action,
-    })),
+        errors: action,
+      }),
+    ),
 
-    //pars matches from tournament EESL
-    on(matchActions.parsMatchesFromTournamentEESL, (state): MatchState => ({
-      ...state,
-      matchIsLoading: true,
-    })),
+    on(
+      matchActions.getMatchesByTournamentIdWithPagination,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: true,
+      }),
+    ),
+    on(
+      matchActions.getMatchesByTournamentIDWithPaginationSuccess,
+      (state, action) => {
+        const sortedMatches = [...action.matches].sort((a, b) => {
+          if (a.week !== b.week) {
+            return a.week - b.week;
+          }
+          const dateA = a.match_date ? new Date(a.match_date).getTime() : 0;
+          const dateB = b.match_date ? new Date(b.match_date).getTime() : 0;
+          return dateB - dateA;
+        });
+        return {
+          ...state,
+          matchIsLoading: false,
+          allMatchesInTournamentPaginated: sortedMatches,
+        };
+      },
+    ),
+    on(
+      matchActions.getMatchesByTournamentIDWithPaginationFailure,
+      (state, action): MatchState => ({
+        ...state,
+        matchIsLoading: false,
+        errors: action,
+      }),
+    ),
+
+    on(
+      matchActions.parsMatchesFromTournamentEESL,
+      (state): MatchState => ({
+        ...state,
+        matchIsLoading: true,
+      }),
+    ),
     on(
       matchActions.parsedMatchesFromTournamentEESLSuccessfully,
       (state, action) => {
         let updatedMatchList: IMatchWithFullData[] = [];
         action.parseList.forEach((newMatch) => {
-          let existingMatch = state.allMatchesInTournament.find(
-            (match) => match.id === newMatch.match.id,
+          let existingMatch = state.allMatchesWithFullDataInTournament.find(
+            (match) => match.id === newMatch.id,
           );
 
           if (existingMatch) {
@@ -252,6 +337,8 @@ const matchFeature = createFeature({
   ),
 });
 
+const { selectAll } = adapter.getSelectors();
+
 export const {
   name: matchFeatureKey,
   reducer: matchReducer,
@@ -259,10 +346,14 @@ export const {
   selectMatchIsSubmitting,
   selectCurrentMatchId,
   selectCurrentMatch,
-  selectAllMatches,
   selectAllMatchesInSport,
   selectAllMatchesInTournament,
   selectAllMatchesWithFullDataInTournament,
   selectAllMatchesInTournamentPaginated,
   selectParsedMatchesFromTournamentEESL,
 } = matchFeature;
+
+export const selectAllMatches = createSelector(
+  matchFeature.selectMatchState,
+  selectAll,
+);
