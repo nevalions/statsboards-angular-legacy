9ad5ddf9b448d8de394cd992229164ef0256f021
linroot <nevalions@gmail.com>
2026-01-02 21:51:17 +0300

    feat: Add component tests for Match components (STAF-68)
    - match.component.spec.ts - Basic component class test
    - match-with-full-data.component.spec.ts - Basic component class test
    
    Components tested:
    - MatchComponent - Router outlet wrapper
    - MatchWithFullDataComponent - Match data wrapper

---
+++ src/app/components/match-with-full-data/match-with-full-data.component.spec.ts
@@ -0,0 +1,10 @@
+import { MatchWithFullDataComponent } from './match-with-full-data.component';
+
+describe('MatchWithFullDataComponent', () => {
+  describe('creation', () => {
+    it('should create component class', () => {
+      const component = new MatchWithFullDataComponent();
+      expect(component).toBeTruthy();
+    });
+  });
+});
---
+++ src/app/components/match/match.component.spec.ts
@@ -0,0 +1,10 @@
+import { MatchComponent } from './match.component';
+
+describe('MatchComponent', () => {
+  describe('creation', () => {
+    it('should create component class', () => {
+      const component = new MatchComponent();
+      expect(component).toBeTruthy();
+    });
+  });
+});
