c9c9c39e60371654485f4416c34992044f288744
linroot <nevalions@gmail.com>
2026-01-02 22:09:47 +0300

    feat: Add component tests for PlayerTeamTournament and TeamTournament (STAF-70)
    - item-player-team-tournament.component.spec.ts - Basic component class test
    - team-tournament.component.spec.ts - Basic component class test
    
    Components tested:
    - ItemPlayerTeamTournamentComponent - Player team tournament item wrapper
    - TeamTournamentComponent - Team tournament wrapper
    
    Note: Other components (AddEditPlayerMatchTable, AddEditPlayerToTeamTournament, AddEditTeamTournament, PlayerMatchTable) use inject() and require Angular TestBed setup.

---
+++ src/app/components/player-team-tournament/item-player-team-tournament/item-player-team-tournament.component.spec.ts
@@ -0,0 +1,10 @@
+import { ItemPlayerTeamTournamentComponent } from './item-player-team-tournament.component';
+
+describe('ItemPlayerTeamTournamentComponent', () => {
+  describe('creation', () => {
+    it('should create component class', () => {
+      const component = new ItemPlayerTeamTournamentComponent();
+      expect(component).toBeTruthy();
+    });
+  });
+});
---
+++ src/app/components/team-tournament/team-tournament.component.spec.ts
@@ -0,0 +1,10 @@
+import { TeamTournamentComponent } from './team-tournament.component';
+
+describe('TeamTournamentComponent', () => {
+  describe('creation', () => {
+    it('should create component class', () => {
+      const component = new TeamTournamentComponent();
+      expect(component).toBeTruthy();
+    });
+  });
+});
